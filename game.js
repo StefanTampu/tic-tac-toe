101b00c47398bc4c45c204338300ad8f5a0c1204
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-05-26 12:50:56 -0400

    Create gameFlow()
    The function is currently under development
    So far it creates two players when player vs player is clicked
    Also executes gameBoard() when start is clicked
    Might move parts of gameBoard() into gameFlow()

--- game.js
+++ game.js
@@ -9,15 +9,23 @@ const gameBoard = (() => {
 
     const gameBoxes = document.querySelectorAll(".game-box");
 
+    let turn;
+
     for(let gameBox of gameBoxes){
         gameBox.addEventListener("click", () => {
             boxContent(gameBox.id, "X");
             console.log(board);
             gameBox.textContent = board[gameBox.id];
+            if(turn===true){
+                turn = false;
+            } else {
+                turn = true;
+            }
+            console.log(turn);
         })
     }
     
-})();
+});
 
 function Player(name, symbol){
     this.name = name;
@@ -25,6 +33,20 @@ function Player(name, symbol){
 };
 
 const gameFlow = (() => {
+    const playerList = [];
+
     const optionA = document.getElementById("option-a");
-    optionA.addEventListner("click", )
-})
+    optionA.addEventListener("click", () => {
+        const first = new Player("Player One", "X");
+        const second = new Player("Player Two", "O");
+        playerList.push(first, second);
+    });
+
+    const startButton = document.querySelector(".start-button");
+    startButton.addEventListener("click", () => {
+
+
+
+        gameBoard();
+    })
+})();
